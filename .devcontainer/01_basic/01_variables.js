f9ae2ea0e6b351c3bdfd8750ff882b4f8e697262
iamamit001 <162720014+iamamit001@users.noreply.github.com>
2024-03-08 15:57:27 +0000

    datatype

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,27 @@
+const accountId = 23456
+let account = "hfgc@gmail.com"
+var password = "1234"
+accountCity = "Delhi"
+
+let accountState;
+
+
+//accountId = 2  do not allowed
+
+
+/*
+
+prefer var are not use 
+beacuse the issue of  block scope and  functional scope 
+
+
+*/
+
+
+
+account = "fghf@gmail.com" 
+password= "45677"
+accountCity ="mumbai"
+
+
+console.table([accountId,account,password,accountCity,accountState])
---
+++ .devcontainer/01_basic/datatypes.js
@@ -0,0 +1,25 @@
+"use strict" //  treat all the js code as the new version 
+
+// alert(3 +3 ) it is used in js node not in browzer
+
+console.log(3
+    + 3) // it is also true but we should always write in readable format 
+console.log("amit")
+
+let name = "amit"
+ let number = 18
+ let idBollean = false
+let state;
+
+
+// number 2 power 53
+// bignit
+//string ""
+//bollean
+//null -- standalone value 
+
+// undefined value
+// symbol -- unique 
+// object 
+
+console.log( typeof "amit")
