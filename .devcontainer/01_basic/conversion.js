d199771ea75b7cdf7c1b8ad614631cb47c808c4b
iamamit001 <162720014+iamamit001@users.noreply.github.com>
2024-03-08 16:45:17 +0000

    conversion

---
+++ .devcontainer/01_basic/conversion.js
@@ -0,0 +1,28 @@
+let number = "33"
+
+console.log(typeof number)  // number 
+console.console.log(typeof (number)) // number 
+
+console.log(typeof number) // number = "33" then the type of string
+
+let typeOfNumber = Number(number)
+console.log(typeof number) //  string convert into the number
+
+console.log(typeOfNumber)
+
+// "33" -- 33
+// "33anc" --- nan
+// true -- 1 and false --0
+
+ let score = 5;
+ let typeBollean = Boolean(score)
+ console.log(typeBollean) // it give true when we put the value of score of is empty then the result of the bollean is false
+ // "amit" --- true
+// " " ---- false
+
+
+
+ let somenumber =33
+ let stringType = String(somenumber)
+ console.log(stringType) // it give  us 33
+ console.log(typeof stringType) // it gives us the string type
